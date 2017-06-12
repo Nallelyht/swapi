57c0253ecac0eed0ed4e106eb401e9d971f13f41
Nallely House <nallelyht@gmail.com>
2017-06-12 10:15:02 -0500

    ajax v2

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-var cargarPagina= function() {
+var cargarPagina = function() {
 	cargarPersonajes();
 };
 
 var cargarPersonajes = function(){
-	$.ajax("http://swapi.co/api/people/",{
-		method: "GET",
-		dataType: "json",
-		success: function (response) {
-			var personajes = response.results;
-			var total = response.count;
-			mostrarTotal(total);
-			
-			var $ul = $("#personajes");
-			personajes.forEach(function(personaje){
-				var $li = $("<li/>");
-				$li.text(personaje.name + "-" + personaje.height + " cm");
-				$ul.append($li);
-				console.log(personaje.name);
-			})
-		},
-		error: function (error) {
-			console.log("error", error);
-		}
+	var url = 'http://swapi.co/api/people/'
+	$.get(url, function(response){
+		console.log(response);
+		var personajes = response.results;
+		var total = response.count;
+		mostraTotalPersonajes(total);
+		mostrarPersonajes(personajes);
 	});
-};
-
-var mostrarTotal = function(total){
-	$("#total").text(total);
 }
-$(document).ready(cargarPagina);
+
+	var mostrarPersonajes = function (personajes){
+		var $ul = $("#personajes");
+		personajes.forEach(function(personaje){
+			var $li = $("<li/>");
+			$li.text(personaje.name + "-" + personaje.height + " cm");
+			$ul.append($li);
+		});
+	};
+
+
+	var mostraTotalPersonajes = function(total){
+		$("#total").text(total);
+	};
+	$(document).ready(cargarPagina);
